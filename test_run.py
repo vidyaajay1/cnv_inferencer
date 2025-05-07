cce50cc31db936267fe7b99daf31d60e53f252b3
Vidya Ajay <vajay1@jh.edu>
2025-05-06 20:36:55 -0400

    Update test_run.py with optional simulator code

--- test_run.py
+++ test_run.py
@@ -1,7 +1,11 @@
 import scanpy as sc
 from cnv_inferencer import preprocess_and_cluster, annotate_genomic_positions, call_cnvs
+from cnv_inferencer import simulate_cnvs_on_adata
 
-adata = sc.read_h5ad("files/PBMC_simCNV_2.h5ad")
+
+adata = sc.read_h5ad("files/input_file.h5ad")
+#adata_sim = simulate_cnvs_on_adata(adata, num_cnvs=3, cnv_prob=0.2)
+#adata_sim.write("files/simulated_output.h5ad")
 print(adata.var.columns)
 adata = preprocess_and_cluster(adata)
 adata = annotate_genomic_positions(adata)
