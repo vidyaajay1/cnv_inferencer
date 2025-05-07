25c7a50348c5871feae4adc4fa53344cc93fe15b
Vidya Ajay <vajay1@jh.edu>
2025-05-06 20:35:45 -0400

    Update __init__.py with simulator

--- cnv_inferencer/__init__.py
+++ cnv_inferencer/__init__.py
@@ -1,5 +1,8 @@
 from .preprocessing import annotate_genomic_positions, preprocess_and_cluster
 from .caller import call_cnvs
+from .simulator import simulate_cnvs_on_adata
+
 
 __version__ = "0.1.0"
 __all__ = ["annotate_genomic_positions", "preprocess_and_cluster", "call_cnvs"]
+__all__.append("simulate_cnvs_on_adata")
