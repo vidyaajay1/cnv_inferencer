c411119198d62505915ca82a52176b52b5f2a309
Vidya Ajay <vajay1@jh.edu>
2025-05-07 11:16:32 -0400

    Update test_run.py with comments

--- test_run.py
+++ test_run.py
@@ -4,9 +4,13 @@ from cnv_inferencer import simulate_cnvs_on_adata
 
 
 adata = sc.read_h5ad("files/input_file.h5ad")
+
+##FOR SIMULATION ONLY##
 #adata_sim = simulate_cnvs_on_adata(adata, num_cnvs=3, cnv_prob=0.2)
 #adata_sim.write("files/simulated_output.h5ad")
-print(adata.var.columns)
+#print(adata.var.columns)
+###
+
 adata = preprocess_and_cluster(adata)
 adata = annotate_genomic_positions(adata)
 print(adata.var.columns)
